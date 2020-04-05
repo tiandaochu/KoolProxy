126896bd646d3aee3c1fc738c097ab02cd280db7
ycg31 <ycg31@qq.com>
2020-04-05 16:58:45 +0800

    启用log

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 #alias echo_date='echo $(date +%Y年%m月%d日\ %X):'
 alias echo_date='echo $(date "+%F %T"):'
 
-
+LOGFILE="tmp/koolproxy.log"
 url_cjx="https://shaoxia1991.coding.net/p/cjxlist/d/cjxlist/git/raw/master/cjx-annoyance.txt"
 url_kp="https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/kp.dat"
 url_kp_md5="https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/kp.dat.md5"
@@ -15,14 +15,14 @@ url_fanboy="https://secure.fanboy.co.nz/fanboy-annoyance.txt"
 url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-dnsmasq.conf"
 
 
-    echo_date ------------------- 规则更新 -------------------
-	echo_date ====================================================
-	echo_date 开始更新koolproxy的规则，请等待...
+    echo_date ------------------- 规则更新 ----------------------- >>$LOGFILE
+	echo_date ==================================================== >>$LOGFILE
+	echo_date 开始更新koolproxy的规则，请等待... >>$LOGFILE
 	# 赋予文件夹权限 
 	chmod -R 777 rules
 	# update 中国简易列表 2.0
 	
-		echo_date " ---------------------------------------------------------------------------------------" 
+		echo_date " ---------------------------------------------------------------------------------------"  >>$LOGFILE
 		# wget --no-check-certificate --timeout=8 -qO - $url_easylist > tmp/easylistchina.txt
 		for i in {1..5}; do
 			wget --no-check-certificate --timeout=8 -qO - $url_easylist > tmp/easylistchina.txt
@@ -31,7 +31,7 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 			if [ "$easylistchina_rule_nu_local" -gt 5000 ]; then
 				break
 			else
-				echo_date easylistchina规则文件下载失败
+				echo_date easylistchina规则文件下载失败 >>$LOGFILE
 				koolproxy_basic_easylist_failed=1
 			fi
 		done
@@ -42,7 +42,7 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 			if [ "$cjx_rule_nu_local" -gt 500 ]; then
 				break
 			else
-				echo_date cjx-annoyance规则文件下载失败
+				echo_date cjx-annoyance规则文件下载失败 >>$LOGFILE
 				koolproxy_basic_easylist_failed=1
 			fi
 		done
@@ -52,7 +52,7 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 		#	if [ "$kpr_our_rule_nu_local" -gt 500 ]; then
 		#		break
 		#	else
-		#		echo_date kpr_our_rule规则文件下载失败
+		#		echo_date kpr_our_rule规则文件下载失败 >>$LOGFILE
 		#		koolproxy_basic_easylist_failed=1
 		#	fi
 		#done
@@ -65,27 +65,27 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 		easylist_rules_local=`cat rules/easylistchina.txt  | sed -n '3p'|awk '{print $3,$4}'`
 		easylist_rules_remote=`cat tmp/easylistchina.txt  | sed -n '3p'|awk '{print $3,$4}'`
 
-		echo_date ABP规则的本地版本号： $easylist_rules_local
-		echo_date ABP规则的在线版本号： $easylist_rules_remote
+		echo_date ABP规则的本地版本号： $easylist_rules_local >>$LOGFILE
+		echo_date ABP规则的在线版本号： $easylist_rules_remote >>$LOGFILE
 		if [ "$koolproxy_basic_easylist_failed" != "1" ]; then
 			if [ "$easylistchina_rule_local" -gt 10000 ]; then
 				if [ "$easylist_rules_local" != "$easylist_rules_remote" ]; then
-					echo_date 检测到 ABP规则 已更新，现在开始更新...
-					echo_date 将临时的ABP规则文件移动到指定位置
+					echo_date 检测到 ABP规则 已更新，现在开始更新... >>$LOGFILE
+					echo_date 将临时的ABP规则文件移动到指定位置 >>$LOGFILE
 					mv tmp/easylistchina.txt rules/easylistchina.txt
 					koolproxy_https_ChinaList=1
 				else
-					echo_date 检测到 ABP规则本地版本号和在线版本号相同，那还更新个毛啊!
+					echo_date 检测到 ABP规则本地版本号和在线版本号相同，那还更新个毛啊! >>$LOGFILE
 				fi
 			fi
 		else
-			echo_date ABP规则文件下载失败！>>$LOGFILE
+			echo_date ABP规则文件下载失败！>>$LOGFILE >>$LOGFILE
 		fi
 	
 	
 		# update yhosts规则
 	
-		echo_date " ---------------------------------------------------------------------------------------"
+		echo_date " ---------------------------------------------------------------------------------------" >>$LOGFILE
 		for i in {1..5}; do
 			wget --no-check-certificate --timeout=8 -qO - $url_yhosts > tmp/yhosts.txt
 			wget --no-check-certificate --timeout=8 -qO - $url_yhosts1 > tmp/tvbox.txt
@@ -95,112 +95,113 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 			replenish_rules_local=`cat rules/yhosts.txt  | sed -n '2p' | cut -d "=" -f2`
 			replenish_rules_remote=`cat tmp/yhosts.txt | sed -n '2p' | cut -d "=" -f2`
 			mobile_nu_local=`grep -E -v "^!" tmp/yhosts.txt | wc -l`
-			echo_date yhosts规则本地版本号： $replenish_rules_local
-			echo_date yhosts规则在线版本号： $replenish_rules_remote
+			echo_date yhosts规则本地版本号： $replenish_rules_local >>$LOGFILE
+			echo_date yhosts规则在线版本号： $replenish_rules_remote >>$LOGFILE
 			if [ "$mobile_nu_local" -gt 5000 ]; then
 				if [ "$replenish_rules_local" != "$replenish_rules_remote" ]; then
-					echo_date 检测到 yhosts规则 已更新，现在开始更新...
-					echo_date 将临时文件覆盖到原始yhosts文件
+					echo_date 检测到 yhosts规则 已更新，现在开始更新... >>$LOGFILE
+					echo_date 将临时文件覆盖到原始yhosts文件 >>$LOGFILE
 					mv tmp/yhosts.txt rules/yhosts.txt
 					koolproxy_https_mobile=1
 					break
 				else
-					echo_date 检测到yhosts本地版本号和在线版本号相同，那还更新个毛啊!
+					echo_date 检测到yhosts本地版本号和在线版本号相同，那还更新个毛啊! >>$LOGFILE
 				fi
 			else
-				echo_date yhosts文件下载失败！
+				echo_date yhosts文件下载失败！ >>$LOGFILE
 			fi
 		done
 	
 
 	# update 视频规则
-		echo_date " ---------------------------------------------------------------------------------------"
+		echo_date " ---------------------------------------------------------------------------------------" >>$LOGFILE
 		for i in {1..5}; do
 			kpr_video_md5=`md5sum rules/kp.dat | awk '{print $1}'`
 			wget --no-check-certificate --timeout=8 -qO - $url_kp_md5 > tmp/kp.dat.md5
 			#wget -4 -a tmp/upload/kpr_log.txt -O tmp/kp.dat.md5 $url_kp_md5
 			kpr_video_new_md5=`cat tmp/kp.dat.md5 | sed -n '1p'`
-			echo_date 远程视频规则md5：$kpr_video_new_md5
-			echo_date 您本地视频规则md5：$kpr_video_md5
+			echo_date 远程视频规则md5：$kpr_video_new_md5 >>$LOGFILE
+			echo_date 您本地视频规则md5：$kpr_video_md5 >>$LOGFILE
 
 			if [ "$kpr_video_md5" != "$kpr_video_new_md5" ]; then
-				echo_date 检测到新版视频规则.开始更新..........
+				echo_date 检测到新版视频规则.开始更新.......... >>$LOGFILE
 				wget --no-check-certificate --timeout=8 -qO - $url_kp > tmp/kp.dat
 				#wget -4 -a tmp/upload/kpr_log.txt -O tmp/kp.dat $url_kp
 				kpr_video_download_md5=`md5sum tmp/kp.dat | awk '{print $1}'`
-				echo_date 您下载的视频规则md5：$kpr_video_download_md5
+				echo_date 您下载的视频规则md5：$kpr_video_download_md5 >>$LOGFILE
 				if [ "$kpr_video_download_md5" = "$kpr_video_new_md5" ]; then
-					echo_date 将临时文件覆盖到原始 视频规则 文件
+					echo_date 将临时文件覆盖到原始 视频规则 文件 >>$LOGFILE
 					mv tmp/kp.dat rules/kp.dat
 					mv tmp/kp.dat.md5 rules/kp.dat.md5
 					video_md5=$kpr_video_new_md5
 					break
 				else
-					echo_date 视频规则md5校验不通过...
+					echo_date 视频规则md5校验不通过... >>$LOGFILE
 				fi
 			else
-				echo_date 检测到 视频规则 本地版本号和在线版本号相同，那还更新个毛啊!
+				video_md5=$kpr_video_md5
+				echo_date 检测到 视频规则 本地版本号和在线版本号相同，那还更新个毛啊! >>$LOGFILE
 			fi
 		done
 	
 
 	# update fanboy规则
 	
-		echo_date " ---------------------------------------------------------------------------------------"
+		echo_date " ---------------------------------------------------------------------------------------" >>$LOGFILE
 		for i in {1..5}; do
 			#wget -4 -a tmp/upload/kpr_log.txt -O tmp/fanboy.txt $url_fanboy
 			wget --no-check-certificate --timeout=8 -qO - $url_fanboy > tmp/fanboy.txt
 			fanboy_rules_local=`cat rules/fanboy.txt  | sed -n '3p'|awk '{print $3,$4}'`
 			fanboy_rules_remote=`cat tmp/fanboy.txt  | sed -n '3p'|awk '{print $3,$4}'`
 			fanboy_nu_local=`grep -E -v "^!" tmp/fanboy.txt | wc -l`
-			echo_date fanboy规则本地版本号： $fanboy_rules_local
-			echo_date fanboy规则在线版本号： $fanboy_rules_remote
+			echo_date fanboy规则本地版本号： $fanboy_rules_local >>$LOGFILE
+			echo_date fanboy规则在线版本号： $fanboy_rules_remote >>$LOGFILE
 			if [ "$fanboy_nu_local" -gt 15000 ]; then
 				if [ "$fanboy_rules_local" != "$fanboy_rules_remote" ]; then
-					echo_date 检测到新版本 fanboy规则 列表，开始更新...
-					echo_date 将临时文件覆盖到原始 fanboy规则 文件
+					echo_date 检测到新版本 fanboy规则 列表，开始更新... >>$LOGFILE
+					echo_date 将临时文件覆盖到原始 fanboy规则 文件 >>$LOGFILE
 					mv tmp/fanboy.txt rules/fanboy.txt
 					koolproxy_https_fanboy=1
 					break
 				else
-					echo_date 检测到 fanboy规则 本地版本号和在线版本号相同，那还更新个毛啊!
+					echo_date 检测到 fanboy规则 本地版本号和在线版本号相同，那还更新个毛啊! >>$LOGFILE
 				fi
 			else
-				echo_date fanboy规则 文件下载失败！
+				echo_date fanboy规则 文件下载失败！ >>$LOGFILE
 			fi
 		done
 	
 
 	# update AdGuardHome规则
 	
-		echo_date " ---------------------------------------------------------------------------------------"
+		echo_date " ---------------------------------------------------------------------------------------" >>$LOGFILE
 		for i in {1..5}; do
 			#wget -4 -a tmp/upload/kpr_log.txt -O tmp/fanboy.txt $url_fanboy
 			wget --no-check-certificate --timeout=8 -qO - $url_AdGuardHome > tmp/AdGuardHome.txt
 			AdGuardHome_rules_local=`cat rules/AdGuardHome.txt  | sed -n '2p' | cut -d "=" -f2`
 			AdGuardHome_rules_remote=`cat tmp/AdGuardHome.txt  | sed -n '2p' | cut -d "=" -f2`
 			AdGuardHome_nu_local=`grep -E -v "^!" tmp/AdGuardHome.txt | wc -l`
-			echo_date AdGuardHome规则本地版本号： $AdGuardHome_rules_local
-			echo_date AdGuardHome规则在线版本号： $AdGuardHome_rules_remote
+			echo_date AdGuardHome规则本地版本号： $AdGuardHome_rules_local >>$LOGFILE
+			echo_date AdGuardHome规则在线版本号： $AdGuardHome_rules_remote >>$LOGFILE
 			if [ "$AdGuardHome_nu_local" -gt 5000 ]; then
 				if [ "$AdGuardHome_rules_local" != "$AdGuardHome_rules_remote" ]; then
-					echo_date 检测到新版本 AdGuardHome规则 列表，开始更新...
-					echo_date 将临时文件覆盖到原始 AdGuardHome规则 文件
-					mv tmp/AdGuardHome.txt rules/AdGuardHome.txt
+					echo_date 检测到新版本 AdGuardHome规则 列表，开始更新... >>$LOGFILE
+					echo_date 将临时文件覆盖到原始 AdGuardHome规则 文件 >>$LOGFILE
+					mv tmp/AdGuardHome.txt rules/AdGuardHome.txt 
 					koolproxy_https_AdGuardHome=1
 					break
 				else
-					echo_date 检测到 AdGuardHome规则 本地版本号和在线版本号相同，那还更新个毛啊!
+					echo_date 检测到 AdGuardHome规则 本地版本号和在线版本号相同，那还更新个毛啊! >>$LOGFILE
 				fi
 			else
-				echo_date AdGuardHome规则 文件下载失败！
+				echo_date AdGuardHome规则 文件下载失败！ >>$LOGFILE
 			fi
 		done
 	
 
 
 	if [ "$koolproxy_https_fanboy" = "1" ]; then
-		echo_date 正在优化 fanboy规则。。。。。
+		echo_date 正在优化 fanboy规则。。。。。 >>$LOGFILE
 		# 删除导致KP崩溃的规则
 		# 听说高手?都打的很多、这样才能体现技术
 		sed -i '/^\$/d' rules/fanboy.txt
@@ -279,13 +280,13 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 			fi
 		done	
 	else
-		echo_date 跳过优化 fanboy规则。。。。。
+		echo_date 跳过优化 fanboy规则。。。。。 >>$LOGFILE
 	fi
 
 
 
 	if [ "$koolproxy_https_ChinaList" = "1" ]; then
-		echo_date 正在优化 ABP规则。。。。。
+		echo_date 正在优化 ABP规则。。。。。 >>$LOGFILE
 		sed -i '/^\$/d' rules/easylistchina.txt
 		sed -i '/\*\$/d' rules/easylistchina.txt
 		# 给btbtt.替换过滤规则。
@@ -366,14 +367,14 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 		#cat rules/kpr_our_rule.txt >> rules/easylistchina.txt
 
 	else
-		echo_date 跳过优化 ABP规则。。。。。
+		echo_date 跳过优化 ABP规则。。。。。 >>$LOGFILE
 	fi
 
 
 	if [ "$koolproxy_https_mobile" = "1" ]; then
 		# 删除不必要信息重新打包 0-11行 表示从第15行开始 $表示结束
 		# sed -i '1,11d' rules/yhosts.txt
-		echo_date 正在优化 补充规则yhosts。。。。。
+		echo_date 正在优化 补充规则yhosts。。。。。 >>$LOGFILE
 
 		# 开始Kpr规则化处理
 		cat rules/yhosts.txt > rules/yhosts_https.txt
@@ -444,14 +445,14 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 			fi
 		done	
 	else
-		echo_date 跳过优化 补充规则yhosts。。。。。
+		echo_date 跳过优化 补充规则yhosts。。。。。 >>$LOGFILE
 	fi
 
 
 	if [ "$koolproxy_https_AdGuardHome" = "1" ]; then
 		# 删除不必要信息重新打包 0-11行 表示从第15行开始 $表示结束
 		# sed -i '1,11d' rules/AdGuardHome.txt
-		echo_date 正在优化 补充规则AdGuardHome。。。。。
+		echo_date 正在优化 补充规则AdGuardHome。。。。。 >>$LOGFILE
 
 		# 开始Kpr规则化处理
 		cat rules/AdGuardHome.txt > rules/AdGuardHome_https.txt
@@ -517,14 +518,14 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
 			fi
 		done	
 	else
-		echo_date 跳过优化 补充规则AdGuardHome。。。。。
+		echo_date 跳过优化 补充规则AdGuardHome。。。。。 >>$LOGFILE
 	fi
 	# 删除临时文件
 	rm -rf rules/*_https.txt
 	#rm rules/kpr_our_rule.txt
 
-	echo_date 所有规则更新并优化完毕！
-	echo_date ====================================================
+	echo_date 所有规则更新并优化完毕！ >>$LOGFILE
+	echo_date ==================================================== >>$LOGFILE
 	#wget 'https://raw.githubusercontent.com/brokeld/KoolProxyR/master/kp.dat' -q -O rules/kp.dat
     wget 'https://raw.githubusercontent.com/brokeld/KoolProxyR/master/daily.txt' -q -O rules/daily.txt
     wget 'https://raw.githubusercontent.com/brokeld/KoolProxyR/master/koolproxy.txt' -q -O rules/koolproxy.txt
@@ -537,12 +538,14 @@ url_AdGuardHome="https://gitee.com/privacy-protection-tools/anti-ad/raw/master/a
     fanboy_rules_local=`cat rules/fanboy.txt  | sed -n '3p'|awk '{print $3,$4}'`
     yhosts_rules_local=`cat rules/yhosts.txt | sed -n '2p' | cut -d "=" -f2`
 	AdGuardHome_rules_local=`cat rules/AdGuardHome.txt | sed -n '2p' | cut -d "=" -f2`
-    echo_date -------------------easylist      version $easylist_rules_local
-    echo_date -------------------fanboy        version $fanboy_rules_local
-    echo_date -------------------yhosts        version $yhosts_rules_local
-	echo_date -------------------AdGuardHome   version $AdGuardHome_rules_local
-	echo_date -------------------kp.dat        MD5 $video_md5
+    echo_date -------------------easylist      version $easylist_rules_local >>$LOGFILE
+    echo_date -------------------fanboy        version $fanboy_rules_local >>$LOGFILE
+    echo_date -------------------yhosts        version $yhosts_rules_local >>$LOGFILE
+	echo_date -------------------AdGuardHome   version $AdGuardHome_rules_local >>$LOGFILE
+	echo_date -------------------kp.dat        MD5 $video_md5 >>$LOGFILE
 
-	echo_date ------------------- 规则更新成功！ -------------------   
+	echo_date ------------------- 规则更新成功！ -------------------    >>$LOGFILE
+
+	cat  $LOGFILE
 
 	
